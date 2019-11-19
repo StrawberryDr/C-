6d49d211db358e011115ac4ee1cc21548f7ac3c0
StrawberryDr <1471612073@qq.com>
2019-11-19 13:56:05 +0800

    20191119

---
+++ Windows窗体实现加法计算/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == string.Empty || textBox2.Text == string.Empty)
+            {
+                MessageBox.Show("输入不完整");
+                return;
+            }
+            int a = int.Parse(textBox1.Text);
+            int b = int.Parse(textBox2.Text);
+            int c = a + b;
+            textBox3.Text = c.ToString();
+            //this.Visible = false;  //隐藏窗口
+        }
+    }
+}
---
+++ Windows窗体实现加法计算/WindowsFormsDemo/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WindowsFormsDemo
+{
+    partial class FormOpacity
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnAdd = new System.Windows.Forms.Button();
+            this.BtnSub = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnAdd
+            // 
+            this.BtnAdd.Location = new System.Drawing.Point(137, 136);
+            this.BtnAdd.Name = "BtnAdd";
+            this.BtnAdd.Size = new System.Drawing.Size(99, 22);
+            this.BtnAdd.TabIndex = 0;
+            this.BtnAdd.Text = "增加透明度";
+            this.BtnAdd.UseVisualStyleBackColor = true;
+            this.BtnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // BtnSub
+            // 
+            this.BtnSub.Location = new System.Drawing.Point(432, 136);
+            this.BtnSub.Name = "BtnSub";
+            this.BtnSub.Size = new System.Drawing.Size(104, 22);
+            this.BtnSub.TabIndex = 1;
+            this.BtnSub.Text = "降低透明度";
+            this.BtnSub.UseVisualStyleBackColor = true;
+            this.BtnSub.Click += new System.EventHandler(this.BtnSub_Click);
+            // 
+            // FormOpacity
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 344);
+            this.Controls.Add(this.BtnSub);
+            this.Controls.Add(this.BtnAdd);
+            this.Name = "FormOpacity";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "可调节透明度的窗体";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnAdd;
+        private System.Windows.Forms.Button BtnSub;
+    }
+}
+
