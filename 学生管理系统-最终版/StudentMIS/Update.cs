8dc9c6515aa3dc847ea11df1a871138b5f04a190
StrawberryDr <1471612073@qq.com>
2019-11-27 21:24:09 +0800

    20191127

---
+++ 学生管理系统-最终版/StudentMIS/Update.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentMIS
+{
+    public partial class Update : Form
+    {
+        public static string path;
+        public static string info;
+        public Update()
+        {
+            InitializeComponent();
+            //path = @"D:\证件照.gif";
+            //pictureBox1.Image = Image.FromFile(path);
+        }
+
+        private void Update_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void mySubmitBtn_Click(object sender, EventArgs e)
+        {
+            info = "学号：" + tbxid.Text + "\r\n";
+            info += "姓名：" + tbxName.Text + "\r\n";
+            foreach(Control control in gbxSex.Controls)
+            {
+                if((control as RadioButton).Checked)
+                {
+                    info += "性别：" + (control as RadioButton).Text;
+                }
+            }
+            this.Opacity = 0;
+            info += "\r\n爱好：";
+            foreach (Control control in gbxFavor.Controls)
+            {
+                if ((control as CheckBox).Checked)
+                {
+                    info += (control as CheckBox).Text + " ";
+                }
+            }
+            info += "\r\n出生日期：" + dateTimePicker1.Text;
+            info += "\r\n专业：" + comboBox1.Text;
+            Welcome.browserForm = new Browser();
+            Welcome.browserForm.Show();
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox1.Items.Add("2017年度优秀团员");
+            listBox1.Items.Add("2017年度一等奖学金");
+            listBox1.Items.Insert(2, "2017年度校园运动会10000米游泳冠军");
+            listBox1.SelectedIndex = 1;
+            string msg = string.Format("已添加奖惩记录{0}条", listBox1.Items.Count);
+            MessageBox.Show(msg,"提示");
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            toolStripStatusLabelNowTime.Text = string.Format("现在时间：{0}", DateTime.Now.ToLongTimeString());
+            toolStripProgressBar1.PerformStep();
+            if(toolStripProgressBar1.Value == toolStripProgressBar1.Maximum)
+            {
+                toolStripProgressBar1.Value = toolStripProgressBar1.Maximum;
+            }
+        }
+
+        private void myOpenPictureBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ 学生管理系统-最终版/StudentMIS/Welcome.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace StudentMIS
+{
+    partial class Welcome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(115, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(329, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "学生信息管理系统";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(225, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "XX大学";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("宋体", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.linkLabel1.Location = new System.Drawing.Point(246, 205);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(60, 24);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "登录";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Welcome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 334);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Welcome";
+            this.Text = "欢迎";
+            this.Load += new System.EventHandler(this.Welcome_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
