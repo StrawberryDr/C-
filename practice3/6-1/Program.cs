ec0ef0de5243cb7a9df4ec1620635c0b3c5e280d
StrawberryDr <1471612073@qq.com>
2019-12-26 22:09:48 +0800

    20191226

---
+++ practice3/6-1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_1
+{
+    class Program
+    {
+        public class InsertionSorter
+        {
+            public int[] list;
+            public void Sort()
+            {
+                for(int i = 1;i < list.Length; i++)
+                {
+                    int t = list[i];
+                    int j = i;
+                    while ((j > 0) && (list[j - 1] > t))
+                    {
+                        list[j] = list[j - 1];
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
